fbbd37a77fa8ad9d118f8d54f06632dd67659eec
dceoy <dnarsil+github@gmail.com>
2018-12-28 04:21:35 +0900

    Refactor fdra/qvalue.py

--- fdra/qvalue.py
+++ fdra/qvalue.py
@@ -3,12 +3,13 @@
 import pandas as pd
 
 
-def qvalue(pvalues, method='BH'):
-    n = len(pvalues)
-    if method == 'BH':
-        return pd.DataFrame({'pval': pvalues}).sort_values(
-            by='pval', ascending=False
-        ).reset_index().assign(
+def calculate_qvalue(pvalues, method='BH'):
+    df_p = pd.DataFrame({'pval': pvalues}).sort_values(
+        by='pval', ascending=False
+    ).reset_index()
+    n = df_p.shape[0]
+    if method == 'BH':      # Benjamini-Hochberg method
+        return df_p.assign(
             qval=lambda d: (d['pval'] * n / (n - d.index.values)).cummin()
         ).set_index('index', drop=True).sort_index()['qval']
     else:
