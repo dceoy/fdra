01e6ebdc6c329925fcad55bacce56fac079720c8
dceoy <dnarsil+github@gmail.com>
2018-12-28 04:16:56 +0900

    Add methods

--- fdra/qvalue.py
+++ fdra/qvalue.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python
 
+import numpy as np
 import pandas as pd
+from scipy.interpolate import splev, splrep
 
 
-def qvalue(pvalues, method='BH'):
-    n = len(pvalues)
-    if method == 'BH':
-        return pd.DataFrame({'pval': pvalues}).sort_values(
-            by='pval', ascending=False
-        ).reset_index().assign(
+def calculate_qvalue(pvalues, method='BH', tau_interval=0.05):
+    df_p = pd.DataFrame({'pval': pvalues}).sort_values(
+        by='pval', ascending=False
+    ).reset_index()
+    n = df_p.shape[0]
+    if method == 'BH':      # Benjamini-Hochberg method
+        return df_p.assign(
             qval=lambda d: (d['pval'] * n / (n - d.index.values)).cummin()
+        ).set_index('index', drop=True).sort_index()['qval']
+    elif method == 'BY':    # Benjamini-Yekutieli method
+        return df_p.assign(
+            em=np.cumsum(
+                np.reciprocal(np.arange(1, n + 1, dtype='float32'))
+            )[::-1]
+        ).assign(
+            qval=lambda d:
+            (d['pval'] * n / (n - d.index.values) / d['em']).cummin()
+        ).set_index('index', drop=True).sort_index()['qval']
+    elif method == 'ST':   # Storey-Tibshirani method
+        pi0 = pd.DataFrame({
+            'tau': np.arange(start=tau_interval, stop=1, step=tau_interval)
+        }).assign(
+            pi=lambda d: d['tau'].apply(
+                lambda t: np.sum(df_p['pval'] > t) / n / (1 - t)
+            )
+        ).pipe(
+            lambda d: np.float32(splev(1, splrep(d['tau'], d['pi'], k=3)))
+        )
+        return df_p.assign(
+            qval=lambda d: (
+                d['pval'] * pi0 * n / (n - d.index.values)
+            ).cummin()
         ).set_index('index', drop=True).sort_index()['qval']
     else:
         raise RuntimeError('Unimplemented method')
