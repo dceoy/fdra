998eb869b08529efc60e130716679c3ead29cb8e
dceoy <dnarsil+github@gmail.com>
2018-12-28 21:44:11 +0900

    Update fdra/fdr.py

--- fdra/fdr.py
+++ fdra/fdr.py
@@ -7,12 +7,17 @@ import pandas as pd
 
 
 def qvalue(pvalues, method='BH'):
-    df_p = pd.DataFrame({'pval': pvalues}).sort_values(
-        by='pval', ascending=False
-    ).reset_index()
-    n = df_p.shape[0]
+    """Calculate q-values by the Benjamini-Hochberg method
+    Args:
+        pvalues (numpy.ndarray): p-value array
+    Returns:
+        numpy.ndarray: q-value array
+    """
+    n = len(pvalues)
     if method == 'BH':      # Benjamini-Hochberg method
-        return df_p.assign(
+        return pd.DataFrame({'pval': pvalues}).sort_values(
+            by='pval', ascending=False
+        ).reset_index().assign(
             qval=lambda d: (d['pval'] * n / (n - d.index.values)).cummin()
         ).set_index(
             'index', drop=True
