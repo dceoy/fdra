cd28c78fc1eee875131a963220e99a65e0ef94e6
dceoy <dnarsil+github@gmail.com>
2018-12-29 02:23:42 +0900

    Implement Benjamini-Yekutieli method

--- fdra/fdr.py
+++ fdra/fdr.py
@@ -4,23 +4,34 @@
 # https://github.com/dceoy/fdra
 
 import pandas as pd
+import numpy as np
 
 
 def qvalue(pvalues, method='BH'):
     """Calculate q-values by the Benjamini-Hochberg method
     Args:
-        pvalues (numpy.ndarray): p-value array
+        pvalues (list): p-value list or array
+        method (str): `BH` (Benjamini-Hochberg) or `BY` (Benjamini-Yekutieli)
     Returns:
         numpy.ndarray: q-value array
     """
     n = len(pvalues)
-    if method == 'BH':      # Benjamini-Hochberg method
-        return pd.DataFrame({'pval': pvalues}).sort_values(
-            by='pval', ascending=False
-        ).reset_index().assign(
+    df_p = pd.DataFrame({'pval': pvalues}).sort_values(
+        by='pval', ascending=False
+    ).reset_index()
+    if method == 'BH':
+        df_q = df_p.assign(
             qval=lambda d: (d['pval'] * n / (n - d.index.values)).cummin()
-        ).set_index(
-            'index', drop=True
-        ).sort_index()['qval'].values
+        )
+    elif method == 'BY':
+        w = np.sum(np.reciprocal(np.arange(1, n + 1, dtype='float32')))
+        df_q = df_p.assign(
+            qval=lambda d: (d['pval'] * n / (n - d.index.values) * w).cummin()
+        )
     else:
         raise RuntimeError('Unimplemented method')
+    return df_q.set_index(
+        'index', drop=True
+    ).sort_index()['qval'].clip(
+        lower=0, upper=1
+    ).values
--- test/test_qvalue.py
+++ test/test_qvalue.py
@@ -7,15 +7,19 @@ from fdra.fdr import qvalue
 
 
 class QvaluesByR(unittest.TestCase):
-    def test_qvalue(self, precision=10):
+    """Random p-values and their q-values calculated by stats::p.adjust in R
+    """
+    def test_qvalue(self, precision=5):
         df = pd.read_csv(
             os.path.join(os.path.dirname(__file__), 'qvalues_by_r.csv'),
             index_col=None
         ).assign(
-            BH_new=lambda d: qvalue(d['pval'], method='BH')
+            BH_new=lambda d: qvalue(d['pval'], method='BH'),
+            BY_new=lambda d: qvalue(d['pval'], method='BY')
         ).round(precision)
         for id, row in df.iterrows():
             self.assertEqual(row['BH'], row['BH_new'])
+            self.assertEqual(row['BY'], row['BY_new'])
 
 
 if __name__ == '__main__':
